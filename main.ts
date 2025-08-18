ec565fd0d28f2af4a6ff2e84d950fa00b50bc21e
Eric Busboom <eric@civicknowledge.com>
2025-08-17 19:55:23 -0700

    Rebuilt main cutebot control

--- main.ts
+++ main.ts
@@ -13,13 +13,13 @@ input.onButtonPressed(Button.A, function () {
 radiop.onReceiveJoystickMessage(function (payload) {
     if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
         cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
-    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
+    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.D)) {
         cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ff00)
-    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
+    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.E)) {
         cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xffff00)
-    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
+    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.F)) {
         cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x0000ff)
-    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
+    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.Logo)) {
         cuteBot.flashUniqueHeadlights()
     } else {
         cuteBot.setUniqueHeadlights()
