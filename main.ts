b6e894853976c98a16b79ab5b12dffa1e80a413c
Eric Busboom <eric@civicknowledge.com>
2025-08-17 19:52:35 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+/**
+ * Cutebot Receiver
+ * 
+ * This program receives Joystick message from the Joystick transmitter and
+ * 
+ * controls the robot.
+ */
+// Send BotStatus with giraffe+middle C on A, cow+A440 on B (local device only)
+input.onButtonPressed(Button.A, function () {
+    radiop.sendBotStateTone(4, 10, BeatFraction.Whole)
+    radiop.sendBotStateImage(IconNames.Happy)
+})
+radiop.onReceiveJoystickMessage(function (payload) {
+    if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
+    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ff00)
+    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xffff00)
+    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x0000ff)
+    } else if (radiop.buttonPressed(payload, radiop.JoystickButton.C)) {
+        cuteBot.flashUniqueHeadlights()
+    } else {
+        cuteBot.setUniqueHeadlights()
+        radiop.sendBotStateImage(IconNames.Pitchfork)
+    }
+    cuteBot.controlMotors(payload)
+})
+input.onButtonPressed(Button.B, function () {
+    radiop.sendBotStateTone(4, 10, BeatFraction.Whole)
+    radiop.sendBotStateImage(IconNames.Sad)
+})
+serial.writeLine("== Cutebot Main ==")
+cuteBot.setUniqueHeadlights()
+cuteBot.setUniqueRunningLights()
+cuteBot.init()
